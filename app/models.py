4e0e34e5f8c563ef19da8cfb78866b78d4e6c18e
ktheart <kt.heart@gmail.com>
2023-05-17 18:33:15 -0700

    removed followers and following

--- app/models.py
+++ app/models.py
@@ -6,12 +6,6 @@ from flask_login import UserMixin
 from datetime import datetime
 
 
-
-#table of followers
-followers = db.Table('followers',
-            db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-            db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
-
 #post class
 class Post(db.Model):
     post_id = db.Column(db.Integer, primary_key = True)
@@ -64,28 +58,6 @@ class User(db.Model, UserMixin):
             if collection.plant_id == plant_id:
                 return True
         return False
-
-
-    #setting followed and user's relationship
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
-
-    #follow
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
-
-    #unfollow
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
-
-    #
-    def is_following(self, user):
-        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
